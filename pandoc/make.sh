fdc9aed06c0c321fdb99a4d6216f21add966d250
Elias F <32647755+ef4203@users.noreply.github.com>
2019-07-18 01:48:28 +0200

    Added Makefile for pandoc

--- pandoc/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pandoc -s --csl=chicago.csl \
-     --latex-engine=xelatex \
-    --variable documentclass=paper \
-    --bibliography=mybooks.bib \
-    -o ${1//.md}.pdf ${1}
-
-echo "Generated ${1//.md}.pdf"
